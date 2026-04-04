f3d140fdbdb94fb1c8ddb99d4cb13b33a5c1e5fc
Itsnatch <kamaumark996@gmail.com>
2026-04-04 21:34:43 +0300

    pro

--- components/screens/Hero.tsx
+++ components/screens/Hero.tsx
@@ -66,7 +66,7 @@ export default function HeroSection() {
       </div>
 
       {/* Status Bar */}
-      <div className="status-bar absolute top-10 md:top-24 left-4 md:left-24 flex items-center gap-3 z-10">
+      <div className="status-bar absolute top-16 md:top-24 left-4 md:left-24 flex items-center gap-3 z-10">
         <span className="relative flex h-3 w-3">
           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
           <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
